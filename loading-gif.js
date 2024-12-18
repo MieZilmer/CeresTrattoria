f0b1dad82d10078db30d56d4429ceebeab118153
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2024-12-18 04:02:33 +0100

    Menu

---
+++ loading-gif.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+ const loadingScreen = document.getElementById("loading-screen");
+ const links = document.querySelectorAll("a");
+
+ links.forEach(link => {
+     link.addEventListener("click", function(event) {
+         if (link.getAttribute("href") !== "#") {
+             event.preventDefault();
+             loadingScreen.style.display = "flex";
+             setTimeout(() => {
+                 window.location.href = link.href;
+             }, 1700);
+         }
+     });
+ });
+});
