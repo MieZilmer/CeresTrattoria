051eac931a9890a67a5cac2bd2929709f5b4e1c1
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-03-11 15:53:09 +0100

    Retning af små fejl, footer virker, fjernet book bord side

--- book-bord.js
+++ book-bord.js
@@ -1,26 +1,26 @@
-"use strict";
+// "use strict";
 
-function showWhenSection() {
-    document.getElementById('antal-section').style.display = 'none';
-    document.getElementById('when-section').style.display = 'block';
-}
+// function showWhenSection() {
+//     document.getElementById('antal-section').style.display = 'none';
+//     document.getElementById('when-section').style.display = 'block';
+// }
 
-function showKontaktInfoSection() {
-    document.getElementById('when-section').style.display = 'none';
-    document.getElementById('kontakt-info-section').style.display = 'block';
-}
+// function showKontaktInfoSection() {
+//     document.getElementById('when-section').style.display = 'none';
+//     document.getElementById('kontakt-info-section').style.display = 'block';
+// }
 
-function showTakSection() {
-    document.getElementById('kontakt-info-section').style.display = 'none';
-    document.getElementById('tak-section').style.display = 'block';
-}
+// function showTakSection() {
+//     document.getElementById('kontakt-info-section').style.display = 'none';
+//     document.getElementById('tak-section').style.display = 'block';
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const selectElement = document.querySelector("select[name='dag']");
-    for (let day = 1; day <= 31; day++) {
-        const option = document.createElement("option");
-        option.value = day;
-        option.textContent = day;
-        selectElement.appendChild(option);
-    }
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     const selectElement = document.querySelector("select[name='dag']");
+//     for (let day = 1; day <= 31; day++) {
+//         const option = document.createElement("option");
+//         option.value = day;
+//         option.textContent = day;
+//         selectElement.appendChild(option);
+//     }
+// });
